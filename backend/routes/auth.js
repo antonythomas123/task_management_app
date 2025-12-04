704fd7e6cd424cc53d47bb3e10739f251302069f
antonythomas123 <antonythomas993@gmail.com>
2025-12-04 16:24:33 +0530

    generate token fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ import User from "../models/User.js";
 const router = express.Router();
 
 const generateToken = (user) => {
-  jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
 };
